18bebbc1f6eec37af0eaba0c030138971d390b99
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-02-20 15:21:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const memory = [];
 
+//creo le caselle con i numeri
+
 const createCells = function (numberOfCells) {
   const numbersSection = document.getElementById("bingo");
 
@@ -18,24 +20,35 @@ const createCells = function (numberOfCells) {
   }
 };
 
-// random number
+//genero un numero randomico contenuto tra 1 e 76
 
 function genRandomNumber() {
+  //eseguo un controllo dei numeri di modo che non superino il 76
+  if (memory.length >= 76) return null;
+
+  //genero il numero randomico facendo modo che non superi il numero
+  //assegnato alla memoria
   let randomNumber;
 
-  if (memory.length >= 76) return "All numbers drawn!";
-  else {
+  do {
     randomNumber = Math.floor(Math.random() * 76) + 1;
-  }
-  while (memory.includes(randomNumber));
+  } while (memory.includes(randomNumber));
 
   memory.push(randomNumber);
   return randomNumber;
 }
 
+//appendo il numero randomico nel suo contenitore così da
+//renderlo visibile
 let showNumber = function () {
   const extractedNumber = document.getElementById("extractedNumber");
   const randomNumber = genRandomNumber();
+
+  //messaggio in caso dell'estrazione di tutti i numeri
+  if (randomNumber === null) {
+    extractedNumber.innerText = "All numbers have been drawn!";
+    return;
+  }
 
   extractedNumber.innerText = randomNumber;
 
@@ -47,6 +60,8 @@ let showNumber = function () {
   }
 };
 
+//rendo funzionante il pulsante
 document.getElementById("extractButton").addEventListener("click", showNumber);
 
+//creo le celle
 createCells(76);
